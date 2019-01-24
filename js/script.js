688066ada89499c9e2cde7a94f3eca1d835798e2
Nick Weber <webernick@icloud.com>
2019-01-24 15:12:40 -0800

    search bar added

--- js/script.js
+++ js/script.js
@@ -3,14 +3,11 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// jshint esversion:6
-
-
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
 /************************
- * Page Level Constants *
+ * Global Variables     *
  ************************/
 
 // get the entire page
@@ -21,11 +18,20 @@ const studentList = document.querySelector('.student-list');
 const students = document.getElementsByClassName('student-item');
 // get a constant for number of pages I'll need
 const numberOfPages = Math.ceil(students.length / 10)
+// get the page header to append the search bar
+const pageHeader = document.querySelector('.page-header');
 
-// will set dynamically later
+
+// start at page 1
 let page = 1;
 
+// filter on page load
+showPage(page, students);
 
+
+/*******************************************
+ * The Filter Function for Showing Content *
+ * *****************************************/
 function showPage(page, items) {
    let minStudent = (page * 10) - 10;
    let maxStudent = (page * 10) - 1;
@@ -39,7 +45,29 @@ function showPage(page, items) {
    };
 }
 
-showPage(page, students);
+/*/*******************************************
+ * Let's add the search bar                  *
+ * *****************************************/
+
+//  create the div
+const searchDiv = document.createElement('div');
+searchDiv.className = 'student-search';
+
+// create the search input
+const searchInput = document.createElement('input');
+searchInput.placeholder = "Search for students...";
+
+// create the button
+const searchButton = document.createElement('button');
+searchButton.textContent = 'Search';
+
+// append input and button to div
+searchDiv.appendChild(searchInput);
+searchDiv.appendChild(searchButton);
+
+// append to the page header
+pageHeader.appendChild(searchDiv);
+
 
 
 /*****************************************
@@ -78,16 +106,16 @@ function createPagination() {
 }
 createPagination();
 
-/*****************************************
- * Now let's add the button functionality *
- * ***************************************/
+/***********************************************
+ * Now let's add the page button functionality *
+ * *********************************************/
 
  paginationList.addEventListener('click', (e) => {
    e.preventDefault();
    page = e.target.textContent;
    showPage(page, students);
 
-   // reset the links
+   // reset the links by looping to each one
    const linksLength = paginationList.children.length;
    const links = paginationList.children
    for (let i = 0; i < linksLength; i++) {
@@ -96,45 +124,3 @@ createPagination();
    // and add active where it matches
    e.target.className = 'active';
  });
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
